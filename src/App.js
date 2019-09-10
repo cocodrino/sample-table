835f797f5c0b3ffc840aff1558f81fca84249c3b
carlos L <clagccs@gmail.com>
2019-09-10 10:50:51 -0400

    first non functional implementation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import ReactDataGrid from 'react-data-grid';
 import './App.css';
+import ReactDOM from "react-dom";
+const ReactTags = require('react-tag-autocomplete')
+
+class AutoComplete extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      tags: [],
+
+      suggestions: [
+        { id: 3, name: "Bananas" },
+        { id: 4, name: "Mangos" },
+        { id: 5, name: "Lemons" },
+        { id: 6, name: "Apricots" }
+      ]
+    }
+  }
+
+  handleDelete (i) {
+    const tags = this.state.tags.slice(0);
+    tags.splice(i, 1);
+    this.setState({ tags })
+  }
+
+  getValue() {
+    return { title: this.state.tags };
+  }
+
+  getInputNode() {
+    return ReactDOM.findDOMNode(this).getElementsByTagName("input")[0];
+  }
+
+  handleAddition (tag) {
+    const tags = [].concat(this.state.tags, tag)
+    this.setState({ tags }, () => this.props.onCommit())
+  }
+
+  render () {
+    return (
+      <ReactTags
+        tags={this.state.tags}
+        suggestions={this.state.suggestions}
+        handleDelete={this.handleDelete.bind(this)}
+        handleAddition={this.handleAddition.bind(this)} />
+    )
+  }
+}
+
+
+const columns = [
+  { key: 'id', name: 'ID',width:100 },
+  { key: 'title', name: 'Title' , editor : AutoComplete},
+  { key: 'count', name: 'Count' } ];
+
+const rows = [{id: 0, title: '', count: 20}, {id: 1, title: 'row1', count: 40}, {id: 2, title: 'row1', count: 60}];
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let [data, setData] = useState(rows);
+
+  let onGridRowsUpdated = ({ fromRow, toRow, updated }) => {
+    const rows = [...data];
+      for (let i = fromRow; i <= toRow; i++) {
+        rows[i] = { ...rows[i], ...updated };
+      }
+      setData(rows)
+  };
+
+  return (<ReactDataGrid
+    columns={columns}
+    rowGetter={i => data[i]}
+    rowsCount={3}
+    onGridRowsUpdated={onGridRowsUpdated}
+    enableCellSelect={true}
+    minHeight={150} />);
 }
 
 export default App;
